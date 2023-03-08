7d0b679f24e1eb3bd28daee250ba45a6682acf5e
KokoDoko <benni_jolink@hotmail.com>
2023-03-08 17:24:31 +0100

    fix github pages url

--- js/resources.js
+++ js/resources.js
@@ -1,16 +1,19 @@
 import { ImageSource, Sound, Resource, Loader } from 'https://esm.sh/excalibur'
 
+// voordat je upload naar github pages moet je de prefix wisselen
+let prefix = '/excalibur-mario/'
+//let prefix = "../"
+
 const Resources = {
-    Background: new ImageSource("../images/background.png"),
-    Gameover: new ImageSource("../images/gameover.png"),
-    Coin: new ImageSource("../images/coin.png"),
-    Crate: new ImageSource("../images/crate.png"),
-    Ground: new ImageSource("../images/ground.png"),
-    Mario: new ImageSource("../images/mario.png"),
-    Platform: new ImageSource("../images/platform.png"),
-    CoinSound: new Sound("../assets/coin.mp3"),
-    PlatformData: new Resource('../data/platforms.json', 'json'),
-    SomeText: new Resource('../data/test.txt', 'text')
+    Background: new ImageSource(prefix + "images/background.png"),
+    Gameover: new ImageSource(prefix + "images/gameover.png"),
+    Coin: new ImageSource(prefix + "images/coin.png"),
+    Crate: new ImageSource(prefix + "images/crate.png"),
+    Ground: new ImageSource(prefix + "images/ground.png"),
+    Mario: new ImageSource(prefix + "images/mario.png"),
+    Platform: new ImageSource(prefix + "images/platform.png"),
+    CoinSound: new Sound(prefix + "assets/coin.mp3"),
+    PlatformData: new Resource(prefix + "data/platforms.json", "json")
 }
 
 const ResourceLoader = new Loader([
@@ -26,3 +29,5 @@ const ResourceLoader = new Loader([
 ])
 
 export { Resources, ResourceLoader }
+
+// https://maximorlov.com/deploying-to-github-pages-dont-forget-to-fix-your-links/
